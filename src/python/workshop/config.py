d1c13860de66061f01cca6f95fca751678cfe2d9
Dave Glover <dglover@microsoft.com>
2025-08-12 17:20:29 +1000

    updated token and truncation strategy

--- src/python/workshop/config.py
+++ src/python/workshop/config.py
@@ -22,8 +22,8 @@ class Config:
         "/mcp" if (url := os.getenv("DEV_TUNNEL_URL")) else ""
 
     # Model parameters
-    MAX_COMPLETION_TOKENS = 20480
-    MAX_PROMPT_TOKENS = 20480
+    MAX_COMPLETION_TOKENS = 2 * 10240
+    MAX_PROMPT_TOKENS = 6 * 10240
 
     # The LLM is used to generate the SQL queries.
     # Set the temperature and top_p low to get more deterministic results.
