3b90f966582c6000f8b233f1638157329dd16fda
Dave Glover <dglover@microsoft.com>
2025-08-30 14:15:16 +1000

    revert pip install and devtunnel in container

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,26 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Preparing the Python environment..."
-
-python -m pip install --upgrade pip
-
-pip install --user -r requirements.lock.txt
-
-# pip install --user -r src/python/workshop/requirements.txt
-
-# pip install --user -r src/python/mcp_server/sales_analysis/requirements.txt
-
-# pip install --user -r src/shared/webapp/requirements.txt
-
-echo "Python environment setup complete."
-
-echo Installing devtunnels
-curl -sL https://aka.ms/DevTunnelCliInstall | bash
-echo "DevTunnels installed."
-echo "Adding DevTunnels to bashrc"
-echo 'export PATH="/home/vscode/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
-
 echo Setting up Aspire CLI
 curl -sSL https://aspire.dev/install.sh | bash
 
