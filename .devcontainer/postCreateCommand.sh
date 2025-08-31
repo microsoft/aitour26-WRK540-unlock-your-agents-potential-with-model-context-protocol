0f482e20aa147a5e9547de734057f8eeb74f1f7b
Dave Glover <dglover@microsoft.com>
2025-08-31 13:55:56 +1000

    updated container build

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-echo Setting up Aspire CLI
-curl -sSL https://aspire.dev/install.sh | bash
-
-echo "Adding Aspire to the zshrc"
-echo 'export PATH="$HOME/.aspire/bin:$PATH"' >> ~/.zshrc
-source ~/.zshrc
-
 echo Restoring .NET dependencies
 dotnet restore src/csharp
